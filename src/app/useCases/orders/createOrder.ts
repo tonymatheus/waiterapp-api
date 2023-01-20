b35caa724007c2f9749946bb59a9dc13f16a50a3
tonymatheus <tony.chaves14@gmail.com>
2023-01-19 22:41:08 -0300

    Refactor: ✏️ Integrate webSocket with create order

--- src/app/useCases/orders/createOrder.ts
+++ src/app/useCases/orders/createOrder.ts
@@ -1,13 +1,17 @@
 import { Request, Response } from 'express';
 import { Order } from '../../models/Order';
+import { io } from '../../..';
 
 export async function createOrder(req: Request, res: Response) {
   try {
     const { table, products } = req.body;
 
-    const category = await Order.create({ table, products });
+    const order = await Order.create({ table, products });
+    const orderDetails = await order.populate('products.product');
 
-    res.status(201).json(category);
+    io.emit('orders@new', orderDetails);
+
+    res.status(201).json(order);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
